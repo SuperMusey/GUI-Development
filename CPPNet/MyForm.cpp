828001260408270ce82d70974bdf8ecaa1c64029
SuperMusey <rishavde.1729@gmail.com>
2022-01-03 19:34:25 +0530

    Committed

---
+++ CPPNet/MyForm.cpp
@@ -0,0 +1,21 @@
+#include "MyForm.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThread]
+
+void main(array<String^>^ args)
+
+{
+
+    Application::EnableVisualStyles();
+
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    CPPNet::MyForm form;
+
+    Application::Run(% form);
+
+}
